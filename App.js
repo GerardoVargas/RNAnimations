37c4466618b4612c50645a3fa08a9a3bfbce53e2
Gerardo Vargas <evc414@gmail.com>
2020-06-25 13:11:20 -0500

    Animations

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+import Animacion1 from './components/Animacion1'
+import Animacion2 from './components/Animacion2'
+import Animacion3 from './components/Animacion3'
+
+
+const App = () => {
+  return (
+    <>
+     <View style={styles.contenido}>
+        <Animacion3 />
+     </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido: {
+    marginTop: 50
+  }
+});
+
+export default App;
