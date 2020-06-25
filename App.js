a47ce7ce5dafe6b1729dbcaaf104b3e973588e9e
Gerardo Vargas <evc414@gmail.com>
2020-06-25 14:21:13 -0500

    Animacion 5 (spring), presionarBtn added

--- App.js
+++ App.js
@@ -3,19 +3,22 @@ import React from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Platform
 } from 'react-native';
 
 import Animacion1 from './components/Animacion1'
 import Animacion2 from './components/Animacion2'
 import Animacion3 from './components/Animacion3'
 import Animacion4 from './components/Animacion4'
+import Animacion5 from './components/Animacion5'
+
 
 const App = () => {
   return (
     <>
      <View style={styles.contenido}>
-        <Animacion4 />
+        <Animacion5 />
      </View>
     </>
   );
@@ -23,7 +26,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   contenido: {
-    marginTop: 50
+    marginTop: Platform.OS === 'ios' ? 150 : 50
   }
 });
 
