ca7c92666803a6a739e56b8405ee5287920e6285
Gerardo Vargas <evc414@gmail.com>
2020-06-25 15:17:47 -0500

    Animacion 7 added, combine spring and sequence

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import Animacion3 from './components/Animacion3'
 import Animacion4 from './components/Animacion4'
 import Animacion5 from './components/Animacion5'
 import Animacion6 from './components/Animacion6'
+import Animacion7 from './components/Animacion7'
+
 
 
 
@@ -20,7 +22,7 @@ const App = () => {
   return (
     <>
      <View style={styles.contenido}>
-        <Animacion6 />
+        <Animacion7 />
      </View>
     </>
   );
